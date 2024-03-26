f67758dc0e2bce67854bb8250328947c106a8b2a
Fangping Lan <fangpinglan0116@gmail.com>
2024-03-26 03:54:54 +0000

    match results with instance-based alg

--- taxonomies_match/run_match.py
+++ taxonomies_match/run_match.py
@@ -14,7 +14,7 @@ def export_taxonomies(filepath):
         # print(headers)
         # print(df.columns)
         # exit()
-        df.to_csv(f"./data/{sheet_name}.csv", index=False)  
+        df.to_csv(f"./data/instances/{sheet_name}.csv", index=False)  
 
 def match_batch(dfs1, dfs2, matcher, dfs1_names, dfs2_names):
     matches = valentine_match_batch(dfs1, dfs2, matcher, dfs1_names, dfs2_names)
@@ -55,7 +55,7 @@ def df_iter_names():
         yield sheet_name
 
 def get_df_and_names():
-    all_sheets = pd.read_excel("./data/taxonomies-valentine-format.xlsx", sheet_name=None) # sheet_name=None reads all worksheets
+    all_sheets = pd.read_excel("./data/nameonly/taxonomies-valentine-format.xlsx", sheet_name=None) # sheet_name=None reads all worksheets
 
     dfs = []
     names = []
@@ -99,18 +99,18 @@ def store_matches(matches, store_path):
 
 
 if __name__ == '__main__':
-    # excel_file_dir = "./data/taxonomies-valentine-format.xlsx"
+    # excel_file_dir = "./data/instances/taxonomies-valentine-format-instances-easyconcat.xlsx"
     # export_taxonomies(excel_file_dir)
 
-    df1 = pd.read_csv("./data/Zhang2015-06.csv")
-    df2 = pd.read_csv("./data/Zhang2016-11.csv")
+    # df1 = pd.read_csv("./data/instances/Anthonio2020-03.csv")
+    # df2 = pd.read_csv("./data/instances/Du2022-07.csv")
 
-    matcher = SimilarityFlooding()
-    # match(df1, df2, matcher, "Zhang2015", "Zhang2016")
+    matcher = Coma(use_instances=True)
+    # match(df1, df2, matcher, "Anthonio2020", "Du2022")
 
     dfs, names = get_df_and_names()
     mid = len(dfs) // 2
     print(names[:mid])
     print(names[mid:])
     matches = match_batch(dfs[:mid], dfs[mid:], matcher, names[:mid], names[mid:])
-    store_matches(matches, "./matches/similarityflooding/")
+    store_matches(matches, "./matches/coma/instance/")
